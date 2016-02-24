41d69890ec39038272445ee8283d7869cd89e084
Andrey Oprokidnev <oprokidnev@webmotor.ru>
2016-02-24 18:45:09 +0500

    Add creating folder for proxies if not exists

--- src/ProxyManagerModule/Module.php
+++ src/ProxyManagerModule/Module.php
@@ -2,8 +2,31 @@
 
 namespace ProxyManagerModule;
 
-class Module implements \Zend\ModuleManager\Feature\AutoloaderProviderInterface, \Zend\ModuleManager\Feature\ConfigProviderInterface
+class Module implements \Zend\ModuleManager\Feature\AutoloaderProviderInterface,
+    \Zend\ModuleManager\Feature\ConfigProviderInterface
 {
+
+    public function init(\Zend\ModuleManager\ModuleManager $mm)
+    {
+        $mm->getEventManager()->attach(\Zend\ModuleManager\ModuleEvent::EVENT_MERGE_CONFIG, [$this, 'onMergeConfig']);
+    }
+
+    public function onMergeConfig(\Zend\ModuleManager\ModuleEvent $event)
+    {
+        $config = $event->getConfigListener()->getMergedConfig();
+        if (isset($config['proxy_manager_module']['configuration']['proxies_target_dir']) &&
+            $config['proxy_manager_module']['configuration']['proxies_target_dir'] !== null &&
+            !is_dir($config['proxy_manager_module']['configuration']['proxies_target_dir'])
+        ) {
+            $proxyTargetDirectory = $config['proxy_manager_module']['configuration']['proxies_target_dir'];
+            mkdir($proxyTargetDirectory, 0777, true);
+        }
+    }
+
+    public function onBootstrapPre(\Zend\Mvc\MvcEvent $event)
+    {
+        
+    }
 
     /**
      *
